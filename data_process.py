11d4444cb711e9a729cdc6d6e0d8be762e4673ed
Shurui <962900713@qq.com>
2023-11-28 17:21:47 -0500

    process data

--- data_process.py
+++ data_process.py
@@ -2,10 +2,18 @@ import pandas as pd
 import numpy as np
 
 
-# import data from csv file
+# import data from csv file, exclude the first column Date since we will treat it as a time-series data
+data = pd.read_csv('good_cdf.csv').iloc[:, 1:]
 
+# get the missing data positions and the fill-mask
+mask_filled = data.isna().to_numpy().astype(int)
 
+# fill the missing data by the mean value of each column
+data_filled = data.fillna(data.mean())
 
+# normalize the data
+data_normalized = (data_filled - data_filled.min()) / (data_filled.max() - data_filled.min())
 
-
-
+# save the data
+np.save('mask.npy', mask_filled)
+np.save('data.npy', data_normalized)
