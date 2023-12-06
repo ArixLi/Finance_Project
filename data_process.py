1444b0e5d822bb6e32c51d46132e8fb37b43bbc5
Shurui <962900713@qq.com>
2023-12-06 16:49:48 -0500

    change data processing

--- data_process.py
+++ data_process.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 # import data from csv file, exclude the first column Date since we will treat it as a time-series data
-data = pd.read_csv('good_cdf.csv').iloc[:, 1:]
+data = pd.read_csv('good_w_vix.csv').iloc[:, 1:]
 
 # delete open and high price columns, since we only focus on their difference (w.r.t. to open price's ratio)
-data = data.drop(['Open', 'High'], axis=1)
+# same for close.
+data = data.drop(['Open', 'Close', 'High', 'High_Open'], axis=1)
 
 # get the missing data positions and the fill-mask
 mask_filled = data.isna().to_numpy().astype(int)
@@ -14,9 +15,15 @@ mask_filled = data.isna().to_numpy().astype(int)
 # fill the missing data by the mean value of each column
 data_filled = data.fillna(data.mean())
 
-# normalize the data
-data_normalized = (data_filled - data_filled.min()) / (data_filled.max() - data_filled.min())
+# Normalize the feature
+feature = data_filled.iloc[:, :-2]
+feature_normalized = (feature - feature.min()) / (feature.max() - feature.min())
+feature_normalized = feature_normalized.to_numpy().astype(float)
+
+# Add labels
+label = data_filled.iloc[:, -2:].to_numpy().astype(float)
+data_normalized = np.hstack([feature_normalized, label])
 
 # save the data
-np.save('mask.npy', mask_filled)
-np.save('data.npy', data_normalized)
+np.save('mask_vix.npy', mask_filled)
+np.save('data_vix.npy', data_normalized)
