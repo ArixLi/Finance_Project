a86cad646dcf36bc62d37f893a79ccab876be379
Shurui <962900713@qq.com>
2023-12-13 17:52:29 -0500

    Update Baseline

--- baseline.py
+++ baseline.py
@@ -12,13 +12,16 @@ close_open_ratio = (df[:, 1] - df[:, 0]) / df[:, 0]
 
 long_day = []
 
+test_ind = int(len(df) * 2 / 3)
+
 # if S(i-2) >= S(i-1) >= S(i), long at i+i day
-for i in range(3, len(df)):
+for i in range(max(3, test_ind), len(df)):
     if df[i-3, 1] >= df[i-2, 1] >= df[i-1, 1]:
         long_day.append(i)
 
 # Compute PL for the long days
 PL = 0
+daily_pl = np.zeros(len(df)-3)
 for i in long_day:
     if high_open_ratio[i] >= delta:
         PL += high_open_ratio[i] * df[i, 0]
@@ -26,7 +29,5 @@ for i in long_day:
         PL += close_open_ratio[i] * df[i, 0]
 
 print(PL)
-
-
 
 
